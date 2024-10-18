296c7649d900f4d9727051b760543eaa58113464
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2024-10-17 20:27:14 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/caribbean_food_group_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.caribbean_food_group_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:caribbean_food_group_game/scr/app.dart';
+import 'package:flutter/material.dart';
+
+void main() async{
+  runApp(const MyApp());
+}
+
