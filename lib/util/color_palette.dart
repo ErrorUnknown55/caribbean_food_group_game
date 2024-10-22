0d6840adaf133b572b8e0c9227d1d16c7749f837
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2024-10-21 20:21:40 -0400

    Setting up color theme

---
+++ lib/util/color_palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Map<int, Color> ColorPalette = {
+  50: Color(0xffffffff), // White (primary)
+  100: Color(0xFF9BF272),
+  150: Color(0xFF7ABF5A),
+  200: Color(0xFFBAC8D9),
+
+};
+
+MaterialColor customScheme = const MaterialColor(0xffffffff, ColorPalette);
