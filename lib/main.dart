27e2cdcf62418ac7957c213b34f3bdac0101dba1
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2024-11-13 11:20:55 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:caribbean_food_group_game/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Set the app to full screen
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+  // Ensure the app is locked in landscape mode
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+
   runApp(const MyApp());
 }
 
